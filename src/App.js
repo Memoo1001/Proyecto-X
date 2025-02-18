ff0f18dd8be55def2eeddf967cc5f58de7b0ae03
Memoo1001 <Memoo1001@github.com>
2025-02-17 20:01:17 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to WindSurf App</h1>
+      <p>This is a React-based application.</p>
+    </div>
+  );
+}
+
+export default App;
